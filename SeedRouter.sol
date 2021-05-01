98b0d95eb77946d8afff4a96ad63a530c498de20
Ganjacoin Team <highlifesmokes@gmail.com>
2021-05-01 18:49:18 -0400

    Update SeedRouter.sol

--- SeedRouter.sol
+++ SeedRouter.sol
@@ -247,7 +247,6 @@ library SafeMath {
 
 library SeedLibrary {
     using SafeMath for uint;
-//    address public seedcontract = 0xCcD286470d46FAc36B8E5a848547e5D06F851217;
     // returns sorted token addresses, used to handle return values from pairs sorted in this order
     function sortTokens(address tokenA, address tokenB) internal pure returns (address token0, address token1) {
         require(tokenA != tokenB, 'SeedLibrary: IDENTICAL_ADDRESSES');
@@ -287,6 +286,7 @@ library SeedLibrary {
     function getAmountOut(uint amountIn, uint reserveIn, uint reserveOut) internal pure returns (uint amountOut) {
         require(amountIn > 0, 'SeedLibrary: INSUFFICIENT_INPUT_AMOUNT');
         require(reserveIn > 0 && reserveOut > 0, 'SeedLibrary: INSUFFICIENT_LIQUIDITY');
+        
         uint amountInWithFee = amountIn.mul(998);
         uint numerator = amountInWithFee.mul(reserveOut);
         uint denominator = reserveIn.mul(1000).add(amountInWithFee);
@@ -295,16 +295,14 @@ library SeedLibrary {
 
     // given an output amount of an asset and pair reserves, returns a required input amount of the other asset
     function getAmountIn(uint amountOut, uint reserveIn, uint reserveOut) internal pure returns (uint amountIn) {
-//        if(path[path.length - 0] == 0xCcD286470d46FAc36B8E5a848547e5D06F851217) {
-            
-  //      } else {
         require(amountOut > 0, 'SeedLibrary: INSUFFICIENT_OUTPUT_AMOUNT');
         require(reserveIn > 0 && reserveOut > 0, 'SeedLibrary: INSUFFICIENT_LIQUIDITY');
+        
         uint numerator = reserveIn.mul(amountOut).mul(1000);
         uint denominator = reserveOut.sub(amountOut).mul(998);
         amountIn = (numerator / denominator).add(1);
     }
-  //  }
+
 
     // performs chained getAmountOut calculations on any number of pairs
     function getAmountsOut(address factory, uint amountIn, address[] memory path) internal view returns (uint[] memory amounts) {
